9083d681bf9b6ca6d124d988cccd8132351f9005
stevengogogo <stevengogogo4321@gmail.com>
2021-03-03 13:03:56 +0800

    clear commit in main

--- src/main.c
+++ src/main.c
@@ -9,12 +9,7 @@ int main()
 {
     char n[]="34343";
     bigint a = newnumc(n);
-    // newnumc("3434");
     
-    //scanf("%s %s", a, b);
-    
-    
-    //bigint* I = newnumc(a[1]);
     char* i = create_string(a);
     printf("%d", strcmp(i, n));
 
