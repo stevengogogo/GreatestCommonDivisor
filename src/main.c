559c5e8ee3165135dee7b1d354d3b867e2fbecb3
stevengogogo <stevengogogo4321@gmail.com>
2021-03-03 12:17:43 +0800

    move code from old folder

--- src/main.c
+++ src/main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include "include/utils.h"
+#include <string.h>
+#include "include/bigint.h"
+
+#define MAX_DEC 256
 
 int main()
 {
-    
+    char n[]="34343";
+    bigint a = newnumc(n);
     // newnumc("3434");
-    int a = 1;
-    int b = 3;
-    extm big;
+    
     //scanf("%s %s", a, b);
-    big = max(&a, &b);
-
+    
+    
     //bigint* I = newnumc(a[1]);
-    printf("Compare a(=%d) and b(=%d): maximum equals to %d\n",a,b,big.extreme);
+    char* i = create_string(a);
+    printf("%d", strcmp(i, n));
 
     return 0;
 }
--- test/runtest.c
+++ test/runtest.c
@@ -2,11 +2,17 @@
 
 #include "acutest.h"
 
+#include "test_bigint.h"
 #include "test_utils.h"
 
 
 // Run TESTs 
 TEST_LIST = {
+    {"Initiation of Bigint", test_init_bigint},
+    {"Generate Integers to Bigint",test_create_bigint_from_ints},
+    { "Element of BigInt", test_BigInt_element },
+    {"Display", test_display},
     {"Utility", test_utils},
+    {"Utility: string comparison",test_str_identical},
     {NULL, NULL} // Terminate the test
 };
