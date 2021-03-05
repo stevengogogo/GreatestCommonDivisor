05e0d00d82191b648d9a2ecf117e38827833d309
stevengogogo <stevengogogo4321@gmail.com>
2021-03-05 15:02:49 +0800

    bug: swap function

--- src/main.c
+++ src/main.c
@@ -7,20 +7,10 @@
 
 int main()
 {
-    bigint a = newnumc("2343242423423424");
-    //bigint b = newnumc("9990");
-    //bigint ans = newnumc("23408991810000005760");
-    bigint b = newnumc("234324244");
-    bigint ans = newnumc("549078509377421720691456");
-
-    /*
-            2343242423423424
-                   234324244
-    549078509377421720691456
-    539078409377421720691456
-    */
-
-    bigint ans_p = multiplication_bigint(a,b);
+   bigint a = newnumc("23532");
+    bigint b = newnumc("23332");
+    bigint gcd_real = newnumc("8");
+    bigint gcd = gcd_bigint(a,b);
 
 
     return 0;
